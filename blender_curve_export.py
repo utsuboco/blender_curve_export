d2cdb8ce3355414041d6160f7bf52efa9ea5464a
Renaud Rohlinger <renaud.rohlinger@gmail.com>
2021-08-29 11:13:36 +0900

    Create blender_curve_export.py

---
+++ blender_curve_export.py
@@ -0,0 +1,36 @@
+import bpy
+import os
+
+
+currPath = os.path.splitext(bpy.data.filepath)[0]+ ".curves.js"
+file = open(currPath, "w") 
+
+file.write('const curve = {\n')
+for ob in bpy.data.objects.values() : 
+  if ob.type == 'CURVE' :
+    file.write( '"%s":\n' % ob.name)
+    for spline in ob.data.splines :
+      curvetype = spline.type
+      print('curve type:', curvetype)
+
+      if curvetype == 'NURBS' or 'POLY':
+          if len(spline.points) > 0 :
+            file.write("[")
+            for bezier_point in spline.points.values() : 
+              co           = bezier_point.co
+              file.write("[%.3f, %.3f, %.3f],  " % (co.x, co.y, co.z ))
+
+      if curvetype == 'BEZIER':
+          if len(spline.bezier_points) > 0 :
+            file.write("[")
+            for bezier_point in spline.bezier_points.values() : 
+              co           = bezier_point.co
+              file.write("[%.3f, %.3f, %.3f],  " % (co.x, co.y, co.z ))
+            
+
+    file.write("],\n")
+file.write("}\n")
+file.write("\n")
+file.write("export default curve")
+file.write("\n")
+file.close()
